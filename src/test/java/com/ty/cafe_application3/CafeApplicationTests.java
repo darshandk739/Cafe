2e8ee9e648ae4604ebf24953c218c1cd96cec75b
darshandk739 <117796623+darshandk739@users.noreply.github.com>
2023-02-21 15:14:33 +0530

    cafe

---
+++ src/test/java/com/ty/cafe_application3/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.cafe_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
